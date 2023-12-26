2248ff6ce345915589796185842131fda47b7ba9
Oksana Kyryienko <kseniyals1609@gmail.com>
2023-12-26 16:39:10 +0200

    lang fix2

--- src/utils/currentDate.js
+++ src/utils/currentDate.js
@@ -1,9 +1,18 @@
+// export function getCurrentDate() {
+//   const currentDate = new Date()
+//   const day = currentDate.getDate()
+//   const month = currentDate.toLocaleString('default', { month: 'short' }).toLowerCase()
+//   const translatedMonth = this.$t('data.shortMonths.' + month)
+ 
+//   return `${day} ${translatedMonth}`
+// }
+
 export function getCurrentDate() {
   const currentDate = new Date()
   const day = currentDate.getDate()
-  const month = currentDate.toLocaleString('default', { month: 'short' }).toLowerCase()
-  const translatedMonth = this.$t('data.shortMonths.' + month)
- 
+  const month = currentDate.toLocaleString('en-US', { month: 'short' })
+  console.log(month)
+  const translatedMonth = this.$t('data.shortMonths.' + month.toLowerCase())
+
   return `${day} ${translatedMonth}`
 }
-
