37ae984d2d4d35d277de1cb9324520225d99374e
Oksana Kyryienko <kseniyals1609@gmail.com>
2023-12-26 16:44:11 +0200

    delete comments

--- src/utils/currentDate.js
+++ src/utils/currentDate.js
@@ -1,12 +1,3 @@
-// export function getCurrentDate() {
-//   const currentDate = new Date()
-//   const day = currentDate.getDate()
-//   const month = currentDate.toLocaleString('default', { month: 'short' }).toLowerCase()
-//   const translatedMonth = this.$t('data.shortMonths.' + month)
- 
-//   return `${day} ${translatedMonth}`
-// }
-
 export function getCurrentDate() {
   const currentDate = new Date()
   const day = currentDate.getDate()
